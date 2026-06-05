467547ede4cf70e6a92d9388820bd100b273ed1b
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-06-05 13:17:07 -0600

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,48 @@
+from math import ceil
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        highest_pile = max(piles)
+        speeds = (1, highest_pile+1)
+        return self.aux_minEatingSpeed(piles, h,speeds)
+    
+    def aux_minEatingSpeed(self, piles: List[int], h: int, speeds: Tuple[int]) -> int:
+        N = speeds[-1] - speeds[0]
+        if N == 1:
+            return speeds[0]
+
+        if self.evaluate_k(speeds[0] + N//2, piles) > h:
+            return self.aux_minEatingSpeed(piles, h, (speeds[0] + N//2 + 1, speeds[-1]))
+        if self.evaluate_k(speeds[0] + N//2, piles) <= h:
+            if self.evaluate_k(speeds[0] + N//2 - 1, piles) > h:
+                return speeds[0] + N//2
+            else:
+                return self.aux_minEatingSpeed(piles, h, (speeds[0], speeds[0] + N//2))
+
+
+    def evaluate_k(self, k_hat, piles):
+        cumulate_h = 0
+        for p in piles:
+            if k_hat >= p:
+                cumulate_h += 1
+            else:
+                cumulate_h += ceil(p / k_hat)
+        return cumulate_h
+
+    # def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+    #     highest_pile = max(piles)
+        
+    #     for k_hat in range(1, highest_pile+1):
+    #         cumulate_h = 0
+    #         for p in piles:
+    #             if k_hat >= p:
+    #                 cumulate_h += 1
+    #             else:
+    #                 cumulate_h += ceil(p / k_hat)
+    #             if cumulate_h > h:
+    #                 break
+    #         if cumulate_h <= h:
+    #             return k_hat
+
+    #     return k_hat
+    
