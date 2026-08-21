fa544c1f933380e6fb1c77b0da62a495adb8ff6a
Miguel Liera <53274605+MiguelAngelLiera@users.noreply.github.com>
2026-08-21 09:52:36 -0600

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        sums = defaultdict(int)
+        p_sum = 0
+        res = 0
+        for i,n in enumerate(nums):
+            p_sum += n 
+            if p_sum == k:
+                res += 1
+        
+            c=sums[p_sum-k]
+            if c:
+                res += c
+            sums[p_sum] += 1
+        return res
+                    
+
+
+        
